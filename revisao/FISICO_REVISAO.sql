fd9f01b558d2a1a882b26715451e30480e0e39d4
Marcus Vinícius Bizerra Moreira <100208378+MarcusVBMoreira@users.noreply.github.com>
2022-08-05 08:59:28 -0300

    Add files via upload

---
+++ revisao/FISICO_REVISAO.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE revisao;
+USE revisao;
+
+CREATE TABLE produto (
+codigo int PRIMARY KEY AUTO_INCREMENT,
+nome_produto varchar(255),
+preco decimal(6,2),
+cor varchar(255)
+);
+
+CREATE TABLE clientes (
+email varchar(255),
+id_cliente int PRIMARY KEY AUTO_INCREMENT,
+estado varchar(2),
+nome_cliente varchar(255)
+);
+
+CREATE TABLE compras (
+nf int PRIMARY KEY AUTO_INCREMENT,
+data_compra date,
+id_cliente int,
+codigo int,
+FOREIGN KEY(id_cliente) REFERENCES clientes (id_cliente),
+FOREIGN KEY(codigo) REFERENCES produto (codigo)
+);
+
